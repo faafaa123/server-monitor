04206ee93d0e1bd546c8b3476e24b17cdd5c0bcf
Fabius <pudlich.fabius@web.de>
2026-02-17 21:57:07 +0100

    create intefaces, pass data instead of socket

--- src/providers/SocketContext.tsx
+++ src/providers/SocketContext.tsx
@@ -1,6 +1,48 @@
 import { io, Socket } from "socket.io-client";
 import { createContext } from "react";
 
+export let data: {
+
+  serverStats: {
+
+    'cpu': number,
+
+    'ram': number,
+
+    'disk': number,
+
+  },
+
+  mongoDb: {
+
+    collectionName: string,
+
+    storageSize: number,
+
+    unit: number,
+
+  }[],
+
+  application: {
+
+    totalNumberOfChunks: number,
+
+    createableChunks: number,
+
+    squareMetersCurrent: number,
+
+    squareMetersPotencial: number,
+
+    creatingChunks: number,
+
+    chunksToCreate: number,
+
+  },
+
+  dynamicObjects: any
+
+}
+
 export const socket: Socket = io("https://www.odrivee.de:3002", {
   autoConnect: false
 });
@@ -20,6 +62,6 @@ socket.on("disconnect", (reason) => {
 
 socket.connect();
 
-export const SocketContext = createContext<Socket | null>(null);
+export const SocketContext = createContext<any | null>(null);
 
 
