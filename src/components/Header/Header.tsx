4733a3d10d21cf602a19d7fb2449147f724c1319
Fabius <pudlich.fabius@web.de>
2026-02-18 22:37:34 +0100

    improve header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,26 @@
 import { Field, ProgressBar } from "@fluentui/react-components";
 import './Header.css'
-
+import favicon from '../../assets/favicon.png'
 
 
 export default function Header() {
 
-
     return (
+
         <header>
+
+            <img style={{width: "48px", position: "absolute", left: "32px"}} src={favicon} alt="Chip" />
+
+            <h1 style={{position: "absolute", left: "100px", top: "10px", fontSize: "36px"}}>oDrivee.de</h1>
+
             <h1>Server and Application Monitor</h1>
+
             <Field validationState="none">
                 <ProgressBar />
             </Field>
+
         </header>
+
     )
 
 }
